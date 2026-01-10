8931b33fa2b12c28cbcf5266d079b1d664c53269
vishnuprabhakar2001 <115687030+vishnuprabhakar2001@users.noreply.github.com>
2026-01-10 21:47:51 +0530

    ... three dots is in authorize to store the multiple parameters as array element

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,8 +1,8 @@
 import asyncHandler from "express-async-handler";
 import { constants } from "../constants.js";
 
-export const authorize = (...allowedRoles) =>
-  asyncHandler(async (req, res, next) => {
+export const authorize = (...allowedRoles) =>   // Here, ...allowedRoles is a REST PARAMETER, not the spread operator. Meaning: “Collect all arguments passed to this function and store them in a single array called allowedRoles.”
+  asyncHandler(async (req, res, next) => {      // The three dots (...) are used to collect multiple parameters passed to a function and store them as elements in a single array.
 
     if (!req.user) {
       res.status(constants.UNAUTHORIZED);
