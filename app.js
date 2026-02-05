bbeac70f2e5d525abd643c5e971dcc01c2f9030f
vishnuprabhakar2001 <115687030+vishnuprabhakar2001@users.noreply.github.com>
2026-02-05 12:50:38 +0530

    app.js is updated

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
 
 app.use("/api/tasks", taskRoutes);
-
 
 export default app;
