7070569ac0c1f787f2aef22f35e0c2f79ea4a2ba
vishnuprabhakar2001 <115687030+vishnuprabhakar2001@users.noreply.github.com>
2026-02-05 11:51:23 +0530

    Fix multer storage for cloud deployment

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,15 +1,22 @@
 import multer from "multer";
 
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");   
-  }, 
+export const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
+});
 
-  filename: function (req, file, cb) {
-    cb(null, file.originalname); 
-  } 
-});     
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
 
-export const upload = multer({ storage });
-   
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   }
+// });
+
+// export const upload = multer({ storage });
